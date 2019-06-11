57e808d101aee5059a7ecec1a178cd7a7852470f
Ian Lyons <ian.thomas.lyons@gmail.com>
2019-06-11 14:34:34 -0500

    search function

--- app/controllers/bus_stops_controller.rb
+++ app/controllers/bus_stops_controller.rb
@@ -24,6 +24,12 @@ class BusStopsController < ApplicationController
 
   
   def search
+      if params[:search].blank?
+          redirect_to(root_path, alert: "Empty field!") and return
+          else
+          @parameter = params[:search].downcase
+          @results = BusStop.all.where("lower(name) LIKE :search", search: "%#{@parameter}%")
+      end
   end
   
   private
